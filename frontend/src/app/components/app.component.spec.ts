98585e884182c5283990e70690c1ebc29389e28d
György Balássy <balassy@users.noreply.github.com>
2017-10-14 07:34:36 +0200

    FIXED: DI at AppComponent unit tests. See Issue #4.

--- frontend/src/app/components/app.component.spec.ts
+++ frontend/src/app/components/app.component.spec.ts
@@ -1,27 +1,42 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { mock, instance, reset, verify } from 'ts-mockito';
+import { MockComponent } from 'ng2-mock-component';
+
 import { AppComponent } from './app.component';
+import { FacebookClientService } from './../services/facebook-client/facebook-client.service';
+
 describe('AppComponent', () => {
+  const mockFacebookClientService: FacebookClientService = mock(FacebookClientService);
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MockComponent({ selector: 'app-navbar' }),
+        MockComponent({ selector: 'app-spinner' })
       ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
+      imports: [ RouterTestingModule ],
+      providers: [
+        { provide: FacebookClientService, useFactory: () => instance(mockFacebookClientService) }
+      ],
+    })
+    .compileComponents();
+
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    component = fixture.debugElement.componentInstance;
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+  afterEach(() => {
+    reset(mockFacebookClientService);
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize the Facebook client', () => {
+    verify(mockFacebookClientService.init()).once();
+  });
 });
