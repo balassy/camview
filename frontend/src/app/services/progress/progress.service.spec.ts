c192ea6fa1bcbbcd5953601e4ab784fc7c2a7f66
György Balássy <balassy@users.noreply.github.com>
2017-10-17 04:55:22 +0200

    ADDED: Unit tests to the ProgressService. See Issue #4.

--- frontend/src/app/services/progress/progress.service.spec.ts
+++ frontend/src/app/services/progress/progress.service.spec.ts
@@ -1,15 +1,42 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import * as Chance from 'chance';
 
 import { ProgressService } from './progress.service';
+import { ProgressState, ProgressStatus } from './progress.types';
 
-describe('ProgressService', () => {
+const chance: Chance.Chance = new Chance();
+
+fdescribe('ProgressService', () => {
+  let service: ProgressService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ProgressService]
     });
+    service = TestBed.get(ProgressService);
   });
 
-  it('should be created', inject([ProgressService], (service: ProgressService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  describe('start function', () => {
+    it('should notify subscribers', () => {
+      const testMessage: string = chance.sentence();
+      service.start(testMessage);
+      service.onStateChanged.subscribe((newState: ProgressState) => {
+        expect(newState.message).toEqual(testMessage);
+        expect(newState.status).toEqual(ProgressStatus.Working);
+      });
+    });
+  });
+
+  describe('end function', () => {
+    it('should notify subscribers', () => {
+      service.end();
+      service.onStateChanged.subscribe((newState: ProgressState) => {
+        expect(newState.status).toEqual(ProgressStatus.Idle);
+      });
+    });
+  });
 });
